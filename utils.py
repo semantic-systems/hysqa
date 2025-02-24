67cc8dd470c67d70785e1986daadad8fc6733306
Tilahun Abedissa <58974800+huntila@users.noreply.github.com>
2025-02-24 22:25:44 +0100

    Update utils.py

---
+++ utils.py
@@ -0,0 +1,30 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+import json
+
+
+def extruct_values(results):
+    return_result = []
+    for result in results["results"]["bindings"]:
+        converted_result = {}
+        for key, value_info in result.items():
+            value = value_info.get('value')
+            if value:
+                converted_result[key] = value
+        return_result.append(converted_result)
+    return return_result
+
+
+def run_sparql_query(sparql_endpoint, sparql_query, param='', flag=False):
+    if flag:
+        sparql_query = sparql_query % param
+    try:
+        sparql = SPARQLWrapper(sparql_endpoint)
+        sparql.setQuery(sparql_query)
+        sparql.setReturnFormat(JSON)
+        result = sparql.query().convert()
+        return result
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return None
+    return
+
