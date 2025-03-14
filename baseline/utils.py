0811fecfbd8573a1b108b35e042caa6ed92d8b33
Tilahun Abedissa <58974800+huntila@users.noreply.github.com>
2025-03-14 12:28:56 +0100

    add load_json() and write_to_json() functions to utils.py

--- baseline/utils.py
+++ baseline/utils.py
@@ -9,6 +9,23 @@ import re
 import importlib
 
 
+def load_json_data(file_name):
+    try:
+        with open(file_name, 'r') as json_file:
+            data = json.load(json_file)
+        return data['data']
+    except FileNotFoundError:
+        print(f"Error: File '{file_name}' not found.")
+        return []
+
+
+def write_to_json(result, out_file_path):
+    with open(out_file_path, "w", encoding="utf-8") as f:
+        json.dump({"data": result}, f, indent=4)
+
+    print("Successfully written to file!")
+
+
 def get_examples(key, file_path="./examples.json"):
     with open(file_path, 'r', encoding='utf-8') as file:
         examples = json.load(file)
